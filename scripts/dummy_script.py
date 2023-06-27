8893e35d4e3a3c40aa79e657c1e010ee773b532e
N2C <N2cryp@protonmail.com>
2023-06-27 12:54:50 +0200

    (feat) add dummy script

---
+++ scripts/dummy_script.py
@@ -0,0 +1,8 @@
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+
+class dummy_script(ScriptStrategyBase):
+    markets = {"binance_paper_trade": {"BTC-USDT"}}
+
+    def on_tick(self):
+        self.logger().info("hello world")
